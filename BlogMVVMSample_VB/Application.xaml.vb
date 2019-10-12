2f164ceac35f8408ab673f8f838f409492bcd948
AkihiroYamamoto <akiyam0613@gmail.com>
2019-10-12 17:04:36 +0900

    プロジェクト作成、ViewModelベースクラス作成

---
+++ BlogMVVMSample_VB/Application.xaml.vb
@@ -0,0 +1,11 @@
+﻿Class Application
+
+    ''' <summary>プログラム起動</summary>
+    ''' <param name="e">プログラム起動イベントデータ</param>
+    Protected Overrides Sub OnStartup(e As StartupEventArgs)
+
+        MyBase.OnStartup(e)
+
+    End Sub
+
+End Class
---
+++ BlogMVVMSample_VB/MVVM/ViewModelBase.vb
@@ -0,0 +1,35 @@
+﻿Imports System.ComponentModel
+
+Namespace My.MVVM
+
+    ''' <summary>ViewModel.ベースクラス</summary>
+    Public MustInherit Class ViewModelBase
+        Implements INotifyPropertyChanged
+
+        ''' <summary>プロパティ変更イベント</summary>
+        Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+        ''' <summary>PropertyChanged()呼び出し</summary>
+        Protected Overridable Overloads Sub CallPropertyChanged()
+
+            ' 呼び出し元メソッド名をプロパティ名として取得
+            Dim caller As StackFrame = New StackFrame(1)                    ' 呼び出し元メソッド情報
+            Dim methodNames As String() = caller.GetMethod().Name.Split("_") ' 呼び出し元メソッド名
+            Dim propertyName As String = methodNames(methodNames.Length - 1)
+
+            CallPropertyChanged(propertyName)
+
+        End Sub
+
+        ''' <summary>PropertyChanged()呼び出し</summary>
+        ''' <param name="propertyName">イベントを発生させたいプロパティ名</param>
+        Protected Overloads Sub CallPropertyChanged(propertyName As String)
+
+            ' イベント発生
+            RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
+
+        End Sub
+
+    End Class
+
+End Namespace
