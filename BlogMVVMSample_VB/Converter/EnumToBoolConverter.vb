1155694f5cff2761ce6d175d218f10c1cf59aee1
AkihiroYamamoto <akiyam0613@gmail.com>
2019-10-18 11:28:04 +0900

    MVVMサンプル：RadioButton

---
+++ BlogMVVMSample_VB/Converter/EnumToBoolConverter.vb
@@ -0,0 +1,79 @@
+﻿Imports BlogMVVMSample_VB.Data
+Imports System.Globalization
+
+Namespace Converter
+
+    ''' <summary>Enumの対象valueが選択されているかbool判定</summary>
+    <ValueConversion(GetType(JobStatus), GetType(Boolean))>
+    Public Class EnumToBoolConverter
+        Implements IValueConverter
+
+        ''' <summary>Enum -> Boolean</summary>
+        ''' <param name="value">The value produced by the binding source.</param>
+        ''' <param name="targetType">The type of the binding target property.</param>
+        ''' <param name="parameter">The converter parameter to use.</param>
+        ''' <param name="culture">The culture to use in the converter.</param>
+        ''' <returns>String</returns>
+        Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.Convert
+
+            Try
+
+                If Not IsNothing(value) AndAlso Not IsNothing(parameter) Then
+
+                    Return value.ToString().Equals(parameter.ToString())
+
+                Else
+
+                    Return False
+
+                End If
+
+            Catch ex As Exception
+
+                Return False
+
+            End Try
+
+        End Function
+
+        ''' <summary>Boolean -> Enum</summary>
+        ''' <param name="value">The value that is produced by the binding target.</param>
+        ''' <param name="targetType">The type to convert to.</param>
+        ''' <param name="parameter">The converter parameter to use.</param>
+        ''' <param name="culture">The culture to use in the converter.</param>
+        ''' <returns>Enum</returns>
+        Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+
+            Try
+
+                If Not IsNothing(value) AndAlso Not IsNothing(parameter) Then
+
+                    Dim bool As Boolean = value
+
+                    If bool Then
+
+                        Return System.Enum.Parse(targetType, parameter.ToString())
+
+                    Else
+
+                        Return Binding.DoNothing
+
+                    End If
+
+                Else
+
+                    Return Binding.DoNothing
+
+                End If
+
+            Catch ex As Exception
+
+                Return Binding.DoNothing
+
+            End Try
+
+        End Function
+
+    End Class
+
+End Namespace
---
+++ BlogMVVMSample_VB/Forms/View/RadioButtonView.xaml.vb
@@ -0,0 +1,9 @@
+﻿Namespace Forms.View
+
+    ''' <summary>RadioButton表示サンプル.View</summary>
+    Public Class RadioButtonView
+
+    End Class
+
+End Namespace
+
